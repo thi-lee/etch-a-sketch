a9fc7f3756d057fd757e1073d6afd0c3362a154b
Thi Le <thilehy7865@gmail.com>
2020-12-10 17:04:11 +0700

    Created grid and customized it so that it'll not change size as number of rows in the grid changes + style

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 let container = document.querySelector('.container');
 
-function makeRows(rows, cols) {
-    for (c = 1; c <= (rows * cols); c++) {
-      let cell = document.createElement("div");
-    //   cell.textContent = c;
-    //   if (c % 5 == 0) {
-    //       cell.style.cssText = "background: lightcyan;"
-    //   } else if (c % 3 == 0) {
-    //       cell.style.cssText = "background: lightblue;"
-    //   }
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(16, 16);
+let rows = 16;
+
+function makegrid(rows) {
+  container.style.gridTemplateColumns = (`repeat(${rows}, 1fr`);
+  container.style.gridTemplateRows = (`repeat(${rows}, 1fr`);
+  for (i = 1; i <= rows * rows; i++) {
+    let cell = document.createElement('div');
+    container.appendChild(cell).classList.add('grid-item',i);
+  }
+}
+
+makegrid(rows);
+
+
+
+
+
+
+
+
+
+// let container = document.querySelector('.container');
+// let main = document.querySelector('.main');
+// let rset = document.createElement('button');
+
+// function makeRows(rows, cols) {
+//   for (c = 1; c <= (rows * cols); c++) {
+//     let cell = document.createElement("div");
+//     container.appendChild(cell).className = "grid-item";
+//   }
+//   let cells = document.querySelectorAll('.grid-item');
+//   cells.forEach((cell) => {
+
+//   })
+// })}
+
+// // rset button
+// rset.textContent = 'CLEAR';
+// rset.style.cssText = 'background: lightcyan';
+// main.insertBefore(rset, container).className = 'rset';
+
+// rset.addEventListener('click', function() {
+//   let rows = prompt('How many squares per side? 1 - 100', '')
+//   makeRows(rows, rows)
+// })
