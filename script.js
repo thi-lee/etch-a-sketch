49d1809fb8d1d66849501c5cc6afc56d8fec2458
Thi Le <thilehy7865@gmail.com>
2020-12-11 15:48:50 +0700

    Fixed bugs - successfully reset with a slider

--- script.js
+++ script.js
@@ -1,68 +1,60 @@
 let container = document.querySelector('.container');
 let instructions = document.querySelector('.instructions');
-
 let rset = document.createElement('button');
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+let newSet = true;
+if (newSet) {
+  makegrid(16);
+  setHover();
+}
+
 rset.textContent = 'Reset';
 rset.style.font = "bold 20px 'Cedarville Cursive', cursive";
 instructions.appendChild(rset).classList.add('rset');
 
-let rows = 88;
+output.textContent = slider.value;
+slider.addEventListener('input', function() {
+  output.textContent = this.value;
+})
+
+rset.addEventListener('click', function() {
+  rSetHover(slider.value)
+});
+
+// functions
+
+function clearAll() {
+  container.innerHTML = ' ';
+}
 
 function makegrid(rows) {
+  clearAll()
   container.style.gridTemplateColumns = (`repeat(${rows}, 1fr`);
   container.style.gridTemplateRows = (`repeat(${rows}, 1fr`);
   for (i = 1; i <= rows * rows; i++) {
     let cell = document.createElement('div');
-    container.appendChild(cell).classList.add('grid-item');
+    container.appendChild(cell).classList.add('grid-item', i);
   }
-  return container;
+  if (newSet) {newSet = false;}
+  return newSet;
 }
 
-makegrid(rows);
-
-let cells = document.querySelectorAll('.grid-item');
-cells.forEach((cell) => {
-  cell.addEventListener('mouseover', function() {
-    cell.classList.add('hovering');
+function setHover() {
+  let cells = document.querySelectorAll('.grid-item');
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseover', function() {
+      cell.classList.add('hovering');
+    })
   })
-})
+}
 
-rset.addEventListener('click', function() {
+function rSetHover(rows) {
+  let cells = document.querySelectorAll('.grid-item');
   cells.forEach((cell) => {
     cell.classList.remove('hovering');
   })
+  newSet = makegrid(rows);
   makegrid(rows);
-})
-
-
-
-
-
-
-
-
-
-// let container = document.querySelector('.container');
-// let main = document.querySelector('.main');
-// let rset = document.createElement('button');
-
-// function makeRows(rows, cols) {
-//   for (c = 1; c <= (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     container.appendChild(cell).className = "grid-item";
-//   }
-//   let cells = document.querySelectorAll('.grid-item');
-//   cells.forEach((cell) => {
-
-//   })
-// })}
-
-// // rset button
-// rset.textContent = 'CLEAR';
-// rset.style.cssText = 'background: lightcyan';
-// main.insertBefore(rset, container).className = 'rset';
-
-// rset.addEventListener('click', function() {
-//   let rows = prompt('How many squares per side? 1 - 100', '')
-//   makeRows(rows, rows)
-// })
+  setHover();
+}
